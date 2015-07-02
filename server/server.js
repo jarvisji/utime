59c0805072a416b4aa55416c5e79139b06efc025
Jarvis Ji <jarvis.ji@outlook.com>
2015-07-02 23:01:19 +0800

    Create server base on Express.

--- server/server.js
+++ server/server.js
@@ -1,39 +1,14 @@
-var loopback = require('loopback');
-var boot = require('loopback-boot');
-var fs = require('fs');
-var morgan = require('morgan');
-var FileStreamRotator = require('file-stream-rotator')
+var express = require('express');
+var app = express();
 
-var app = module.exports = loopback();
+app.use(express.static('client'));
 
-app.start = function () {
-  // start the web server
-  return app.listen(function () {
-    app.emit('started');
-    console.log('Web server listening at: %s', app.get('url'));
-  });
-};
-
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function (err) {
-  if (err) throw err;
-
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+app.get('/', function (req, res) {
+  res.send('hello world');
 });
 
-// === Configure logs =====================================================
-var logDirectory = __dirname + '/log';
-// ensure log directory exists
-fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
-// create a rotating write stream
-var accessLogStream = FileStreamRotator.getStream({
-  filename: logDirectory + '/access-%DATE%.log',
-  frequency: 'daily',
-  date_format: 'YYYY-MM-DD',
-  verbose: false
+var server = app.listen('3000', function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Server listening at: http://%s:%s', host, port);
 });
-// setup the logger
-app.use(morgan('combined', {stream: accessLogStream}));

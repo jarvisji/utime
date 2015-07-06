0abce7515c2175dcb6d7cc38a91ef982a2a62ef3
Jarvis Ji <jarvis.ji@outlook.com>
2015-07-06 16:54:54 +0800

    The most simple version of user register and login.

---
+++ server/schemas.js
@@ -0,0 +1,72 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+module.exports = {
+  userSchema: new Schema({
+    username: String,
+    password: String,
+    mobile: String,
+    linkedTo: String, // teacher/parent/student
+    linkedId: String,
+    wxCallback: {
+      access_token: String,
+      expires_in: Number,
+      refresh_token: String,
+      openid: String,
+      scope: String,
+      unionid: String
+    }
+  }),
+  classSchema: new Schema({
+    schoolId: String,
+    displayName: String,
+    grade: Number,
+    term: Number,
+    classNumber: Number,
+    headTeacher: {
+      teacherId: String,
+      name: String
+    },
+    registerDate: Date,
+    registerGrade: String
+  }),
+  teacherSchema: new Schema({
+    classIds: [String],
+    name: String,
+    sex: String,
+    birthday: Date,
+    mobile: String,
+    QQ: String,
+    email: String,
+    teachStartDate: Date
+  }),
+  studentSchema: new Schema({
+    classId: String,
+    name: String,
+    sex: String,
+    birthday: Date,
+    parents: [{
+      name: String,
+      relation: String,
+      mobile: String,
+      wxId: String
+    }],
+    homeAddress: String,
+    joinClassDate: Date
+  }),
+  courseSchema: new Schema({
+    name: String,
+    teacher: {
+      teacherId: String,
+      name: String
+    },
+    classId: String,
+    grade: Number,
+    term: Number,
+    dayOfWeek: Number,
+    date: Date,
+    memo: String,
+    startTime: Date,
+    endTime: Date
+  })
+};
--- server/server.js
+++ server/server.js
@@ -1,11 +1,64 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var mongoose = require('mongoose');
+var schemas = require('./schemas');
 var app = express();
 
 app.use(express.static('client'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function (req, res) {
-  res.send('hello world');
+// connect to MongoDB
+mongoose.connect('mongodb://localhost:27000/utime');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log('Connected to MongoDB success.');
+  registerRoutes();
 });
+
+var registerRoutes = function () {
+  app.post('/login', function (req, res) {
+    var loginUser = req.body;
+    if (typeof(loginUser) == 'object' && loginUser.mobile && loginUser.password) {
+      var User = mongoose.model('User', schemas.userSchema);
+      User.findOne({'mobile': loginUser.mobile}, 'password', function (err, user) {
+        if (err) {
+          console.error(err);
+          res.send(err);
+        }
+        if (user.password == loginUser.password) {
+          res.send('success');
+        } else {
+          res.send('fail');
+        }
+      });
+    } else {
+      res.send('Invalid request data');
+    }
+  });
+  app.get('/users', function (req, res) {
+    var User = mongoose.model('User', schemas.userSchema);
+    User.find(function (err, users) {
+      if (err) return console.error(err);
+      res.json(users);
+    });
+  });
+  app.post('/users', function (req, res) {
+    console.log(req.body);
+    var User = mongoose.model('User', schemas.userSchema);
+    var user = new User(req.body);
+    user.save(function (err, data) {
+      if (err) {
+        console.error(err);
+        res.send(err)
+      }
+      console.log('saved user:', data);
+      res.send(data)
+    });
+  });
+};
 
 var server = app.listen('3000', function () {
   var host = server.address().address;
